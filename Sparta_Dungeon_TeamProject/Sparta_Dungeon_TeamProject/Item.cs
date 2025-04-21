10aa132344ba27991e3c057d7473c518f6f47d48
AgathaYi <tjsfid1030@naver.com>
2025-04-21 20:03:44 +0900

    필수구현

--- Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Item.cs
+++ Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Item.cs
@@ -1,13 +1,32 @@
-ï»¿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace SpartaDungeon;
 
-namespace Sparta_Dungeon_TeamProject
+class Item
 {
-    internal class Item
-    {
+    public string Name { get; }
+    public int Type { get; }
+    public int Value { get; }
+    public string Desc { get; }
+    public int Price { get; }
 
+    public string DisplayTypeText
+    {
+        get
+        {
+            return Type == 0 ? "°ø°Ý·Â" : "¹æ¾î·Â";
+        }
+    }
+
+    public Item(string name, int type, int value, string desc, int price)
+    {
+        Name = name;
+        Type = type;
+        Value = value;
+        Desc = desc;
+        Price = price;
+    }
+
+    public string ItemInfoText()
+    {
+        return $"{Name}  |  {DisplayTypeText} +{Value}  |  {Desc}";
     }
 }
--- Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Player.cs
+++ Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Player.cs
@@ -1,13 +1,115 @@
-癤퓎sing System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace SpartaDungeon;
 
-namespace Sparta_Dungeon_TeamProject
+class Player
 {
-    internal class Player
-    {
+    public int Level { get; }
+    public string Name { get; }
+    public string Job { get; }
+    public int Atk { get; }
+    public int Def { get; }
+    public int Hp { get; private set; }
+    public int Gold { get; private set; }
 
+    public int ExtraAtk { get; private set; }
+    public int ExtraDef { get; private set; }
+
+    private List<Item> Inventory = new List<Item>();
+    private List<Item> EquipList = new List<Item>();
+
+    public int InventoryCount
+    {
+        get
+        {
+            return Inventory.Count;
+        }
+    }
+
+    public Player(int level, string name, string job, int atk, int def, int hp, int gold)
+    {
+        Level = level;
+        Name = name;
+        Job = job;
+        Atk = atk;
+        Def = def;
+        Hp = hp;
+        Gold = gold;
+    }
+
+    public void DisplayCharacterInfo()
+    {
+        Console.WriteLine($"Lv. {Level:D2}");
+        Console.WriteLine($"{Name} {{ {Job} }}");
+        Console.WriteLine(ExtraAtk == 0 ? $"공격력 : {Atk}" : $"공격력 : {Atk + ExtraAtk} (+{ExtraAtk})");
+        Console.WriteLine(ExtraDef == 0 ? $"방어력 : {Def}" : $"방어력 : {Def + ExtraDef} (+{ExtraDef})");
+        Console.WriteLine($"체력 : {Hp}");
+        Console.WriteLine($"Gold : {Gold} G");
+    }
+
+    public void DisplayInventory(bool showIdx)
+    {
+        for (int i = 0; i < Inventory.Count; i++)
+        {
+            Item targetItem = Inventory[i];
+
+            string displayIdx = showIdx ? $"{i + 1} " : "";
+            string displayEquipped = IsEquipped(targetItem) ? "[E]" : "";
+            Console.WriteLine($"- {displayIdx}{displayEquipped} {targetItem.ItemInfoText()}");
+        }
+    }
+
+    public void EquipItem(Item item)
+    {
+        if (IsEquipped(item))
+        {
+            EquipList.Remove(item);
+            if (item.Type == 0)
+                ExtraAtk -= item.Value;
+            else
+                ExtraDef -= item.Value;
+        }
+        else
+        {
+            EquipList.Add(item);
+            if (item.Type == 0)
+                ExtraAtk += item.Value;
+            else
+                ExtraDef += item.Value;
+        }
+    }
+
+    public bool IsEquipped(Item item)
+    {
+        return EquipList.Contains(item);
+    }
+
+    public void BuyItem(Item item)
+    {
+        Gold -= item.Price;
+        Inventory.Add(item);
+    }
+
+    public bool HasItem(Item item)
+    {
+        return Inventory.Contains(item);
+    }
+
+    public List<Item> GetInventoryItems()
+    {
+        return Inventory;
+    }
+
+    public void SellItem(Item item)
+    {
+        Gold += (int)(item.Price * 0.85);
+        Inventory.Remove(item);
+        EquipList.Remove(item);
+        if (item.Type == 0) ExtraAtk -= item.Value;
+        else ExtraDef -= item.Value;
+    }
+
+    public void Rest()
+    {
+        Gold -= 500;
+        Hp = 100;
     }
 }
