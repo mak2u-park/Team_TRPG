a26d18571101f3b88aced7459b357e559d26c812
mak2u-park <scorio1234@gmail.com>
2025-04-21 16:01:38 +0900

    Class

---
+++ Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_Dungeon_TeamProject
+{
+    internal class GameManager
+    {
+        public void StartGame()
+        {
+
+        }
+    }
+}
