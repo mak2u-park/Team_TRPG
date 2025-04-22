6bdbef5e621db2f9abebe5e2ca9a1a87b2a7d958
mak2u-park <scorio1234@gmail.com>
2025-04-22 18:55:43 +0900

    몬스터 레벨 비례 스탯 증가
    레벨, orgin 비례 스탯 상승,
    코드 설명 추가

--- Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Monster.cs
+++ Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Monster.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,18 +11,38 @@ namespace Sparta_Dungeon_TeamProject
   
     public class Monster
     {
-
+        // 몬스터 기본 스탯
         public int Level { get;}
         public int Atk { get; }
         public int Def { get; }
         public int Hp { get; }
+
+
+        // 레벨 상승에 따른 최종 스탯 계산을 위한 float값 추가
+        public float FinalAtk { get; }
+        public float FinalDef { get; }
+        public float FinalHp { get; }
+
+
+        // 몬스터 드랍 보상 (골드, 경험치)
         public int DropGold { get; }
         public int DropExp { get; }
+
+        // 몬스터 생존 여부
         public bool IsAlive { get; set; }
+
 
         private static Random random = new Random();
 
-        public Monster(int minLevel, int maxLevel, int minAtk, int maxAtk, int minDef, int maxDef, int minHp, int maxHp, int minDropGold, int maxDropGold, int minDropExp, int maxDropExp, bool isAlive)
+        public Monster(
+            int minLevel, int maxLevel, 
+            int minAtk, int maxAtk, 
+            int minDef, int maxDef, 
+            int minHp, int maxHp, 
+            int minDropGold, int maxDropGold, 
+            int minDropExp, int maxDropExp, 
+            bool isAlive, 
+            float orgin)
         {
             Level = random.Next(minLevel, maxLevel + 1);
             Atk = random.Next(minAtk, maxAtk + 1);
@@ -30,11 +51,27 @@ namespace Sparta_Dungeon_TeamProject
             DropGold = random.Next(minDropGold, maxDropGold + 1);
             DropExp = random.Next(minDropExp, maxDropExp + 1);
             IsAlive = isAlive;
+            FinalAtk = Atk * (1 + orgin * Level);
+            FinalDef = Def * (1 + orgin * Level);
+            FinalHp = Hp * (1 + orgin * Level);
+            /*
+            레벨, 공격력, 방어력, 체력, 드랍골드, 드랍경헙치는 일정 범위 이내에서 랜덤한 int값으로 정해진다.
+            최종 공격력, 방어력, 체력은 레벨과 orgin(태생)에 따라 영향을 받으며 좋은 태생을 가지고 있을수록
+            레벨이 높을때 더 높은 최종 스탯을 갖는다.
+            */
+
         }
+
+        
     }
 
     class Goblin : Monster
     {
+        public float finalAtk { get; }
+        public float finalDef { get; }
+        public float finalHp { get; }
+
+
         public Goblin() : base(
             minLevel: 1, maxLevel: 5,
             minAtk: 3, maxAtk: 5,
@@ -42,9 +79,10 @@ namespace Sparta_Dungeon_TeamProject
             minHp: 10, maxHp: 15,
             minDropGold: 1000, maxDropGold: 3000,
             minDropExp: 50, maxDropExp: 100, 
-            isAlive: true)
-        { 
-
+            isAlive: true,
+            orgin: 0.1f)
+        {
+            
         }
 
     }
@@ -59,12 +97,15 @@ namespace Sparta_Dungeon_TeamProject
             minHp: 10, maxHp: 30,
             minDropGold: 2000, maxDropGold: 5000,
             minDropExp: 50, maxDropExp: 100,
-            isAlive: true)
+            isAlive: true,
+            orgin: 0.15f)
         { 
 
         }
 
     }
 
+
+    
  
 }
