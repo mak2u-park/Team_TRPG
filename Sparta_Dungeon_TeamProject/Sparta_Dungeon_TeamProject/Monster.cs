74ce35c2008cf9de4acc6f387b9bb623e4c3df36
mak2u-park <scorio1234@gmail.com>
2025-04-22 17:11:25 +0900

    몬스터 클래스 작성
    몬스터 클래스를 상속받는 하위 클래스를 작성하여 랜덤 스테이터스를 갖도록 설계하였다.

---
+++ Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Monster.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_Dungeon_TeamProject
+{
+
+  
+    public class Monster
+    {
+
+        public int Level { get;}
+        public int Atk { get; }
+        public int Def { get; }
+        public int Hp { get; }
+        public int DropGold { get; }
+        public int DropExp { get; }
+        public bool IsAlive { get; set; }
+
+        private static Random random = new Random();
+
+        public Monster(int minLevel, int maxLevel, int minAtk, int maxAtk, int minDef, int maxDef, int minHp, int maxHp, int minDropGold, int maxDropGold, int minDropExp, int maxDropExp, bool isAlive)
+        {
+            Level = random.Next(minLevel, maxLevel + 1);
+            Atk = random.Next(minAtk, maxAtk + 1);
+            Def = random.Next(minDef, maxDef + 1);
+            Hp = random.Next(minHp, maxHp + 1);
+            DropGold = random.Next(minDropGold, maxDropGold + 1);
+            DropExp = random.Next(minDropExp, maxDropExp + 1);
+            IsAlive = isAlive;
+        }
+    }
+
+    class Goblin : Monster
+    {
+        public Goblin() : base(
+            minLevel: 1, maxLevel: 5,
+            minAtk: 3, maxAtk: 5,
+            minDef: 5, maxDef: 10,
+            minHp: 10, maxHp: 15,
+            minDropGold: 1000, maxDropGold: 3000,
+            minDropExp: 50, maxDropExp: 100, 
+            isAlive: true)
+        { 
+
+        }
+
+    }
+
+
+    class Orc : Monster
+    {
+        public Orc() : base(
+            minLevel: 3, maxLevel: 5,
+            minAtk: 5, maxAtk: 10,
+            minDef: 10, maxDef: 20,
+            minHp: 10, maxHp: 30,
+            minDropGold: 2000, maxDropGold: 5000,
+            minDropExp: 50, maxDropExp: 100,
+            isAlive: true)
+        { 
+
+        }
+
+    }
+
+ 
+}
