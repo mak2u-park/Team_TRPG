5b325821bb5010db62d4f7e1627daaccc2a59c71
PostMail-hub <gmlals2949@naver.com>
2025-04-23 11:40:19 +0900

    배틀 , 몬스터 클래스 수정
    - 몬스터의 값을 불러올 수 있게 set 설정
    - 기존에 존재한 임시 몬스터 값 삭제
    - 리스트로 만들어져있는 몬스터를 불러오기 기능으로 대체
    - 전투 진행 중 몬스터를 난이도 별로 타입과 마릿수를 설정하여 호출

--- Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Battle.cs
+++ Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Battle.cs
@@ -1,51 +1,24 @@
 ﻿using System.Threading;
 using System.Xml.Linq;
+using static Sparta_Dungeon_TeamProject.Monster;
+using static Sparta_Dungeon_TeamProject.Monster.MonsterFactory;
 
 namespace Sparta_Dungeon_TeamProject
 {
     public partial class Program
     {
-        static string GetMonsterStatus(int hp, bool alive) // 살아있다면 Hp를 표시해주고 죽어있다면 [사망]을 표시해줌
+        public static string GetMonsterStatus(Monster mon)
         {
-            return alive ? $"[HP:{hp}]" : "[사망]";
+            return mon.IsAlive ? $"[HP:{mon.Hp}]" : "[사망]";
         }
-
-        public static int Mhp1 = 10; // 임시로 만든 몬스터의 체력값
-        public static int Mhp2 = 10; // 임시로 만든 몬스터의 체력값
-        public static int Mhp3 = 10; // 임시로 만든 몬스터의 체력값
-
-
-        public static int Mlv1 = 1; // 임시로 만든 몬스터의 레벨값
-        public static int Mlv2 = 2; // 임시로 만든 몬스터의 레벨값
-        public static int Mlv3 = 3; // 임시로 만든 몬스터의 레벨값
-
-
-        public static int Matk1 = 3; // 임시로 만든 몬스터의 공격값
-        public static int Matk2 = 5; // 임시로 만든 몬스터의 공격값
-        public static int Matk3 = 50; // 임시로 만든 몬스터의 공격값
-
-        public static int Monnum = 3; // 임시로 만든 전투용 몬스터 리스트에 남은 몬스터 값
 
 
         public static int KillMon = 0; // 임시로 만든 몬스터 처치 횟수 값
 
-
-        public static bool Mlife1 = true; // 임시로 만든 몬스터의 생존 여부
-        public static bool Mlife2 = true; // 임시로 만든 몬스터의 생존 여부
-        public static bool Mlife3 = true; // 임시로 만든 몬스터의 생존 여부
-
-
         public static bool Playerturn = true; // 플레이어의 턴 여부
 
+        public static List<Monster> battleMonsters = new List<Monster>(); // 전투용 몬스터 리스트
 
-        public static string Mname1 = "슬라임"; // 임시로 만든 몬스터의 명칭
-        public static string Mname2 = "고약한 고블린"; // 임시로 만든 몬스터의 명칭
-        public static string Mname3 = "미니 카피바라"; // 임시로 만든 몬스터의 명칭
-
-
-        public static string MonsterLifeMark1 = Mlife1 ? $"[HP:{Mhp1}]" : "[사망]"; // 임시로 만든 몬스터의 사망 여부에 따른 체력 / 사망 표시 
-        public static string MonsterLifeMark2 = Mlife2 ? $"[HP:{Mhp2}]" : "[사망]"; // 임시로 만든 몬스터의 사망 여부에 따른 체력 / 사망 표시  
-        public static string MonsterLifeMark3 = Mlife3 ? $"[HP:{Mhp3}]" : "[사망]"; // 임시로 만든 몬스터의 사망 여부에 따른 체력 / 사망 표시 
 
         // 4. 던전
         static void DisplayDungeonUI()
@@ -72,6 +45,8 @@ namespace Sparta_Dungeon_TeamProject
         static void Battle()
         {
             bool BettleLoop = true; // 전투 진행 루프를 켜줌
+            battleMonsters = MonsterSpawner.SpawnMonsters("easy"); // ("난이도") 를 조절해서 몬스터 마릿 수와 종류 조절 가능
+            Playerturn = true;
 
             while (BettleLoop) // 전투 진행 루프가 켜져 있다면
             {
@@ -82,10 +57,9 @@ namespace Sparta_Dungeon_TeamProject
                     Console.WriteLine();
                     Console.WriteLine("**현재 전투 중 입니다.**");
                     Console.WriteLine();
-                    // 몬스터가 출력되는 부분은 이후에 인벤토리에서 아이템을 가져온 것 처럼 몬스터 리스트에서 전투용 몬스터 리스트로 몬스터를 출력시켜야 할 듯
-                    Console.WriteLine($"[Lv.{Mlv1}][{Mname1}] {GetMonsterStatus(Mhp1, Mlife1)}");
-                    Console.WriteLine($"[Lv.{Mlv2}][{Mname2}] {GetMonsterStatus(Mhp2, Mlife2)}");
-                    Console.WriteLine($"[Lv.{Mlv3}][{Mname3}] {GetMonsterStatus(Mhp3, Mlife3)}");
+
+                    PrintMonsters(); // 몬스터 출력
+
                     Console.WriteLine();
                     Console.WriteLine("1. 일반 공격");
                     Console.WriteLine("2. 스킬 선택");
@@ -114,26 +88,17 @@ namespace Sparta_Dungeon_TeamProject
                     Console.WriteLine("**현재 전투 중 입니다.**");
                     Console.WriteLine();
                     Thread.Sleep(500);
-
-                    if (Mlife1) // 이를 리스트로 불러와서 전투중인 몬스터의 수 만큼 불러와서 한 마리씩 차례대로 불러온 뒤 타격하도록 설정해야함.
+                    foreach (var m in battleMonsters)
                     {
-                        Console.WriteLine($"[Lv.{Program.Mlv1}][{Program.Mname1}] 가 공격을 시도 합니다!");
-                        player.Damage(Matk1); // 데미지 받는 것 불러오기 , 여기 수치도 리스트 값 처럼 불러와야함
-                        Thread.Sleep(700);
-                    }
 
-                    if (Mlife2)
-                    {
-                        Console.WriteLine($"[Lv.{Program.Mlv2}][{Program.Mname2}] 가 공격을 시도 합니다!");
-                        player.Damage(Matk2); // 데미지 받는 것 불러오기 , 여기 수치도 리스트 값 처럼 불러와야함
-                        Thread.Sleep(700);
-                    }
-
-                    if (Mlife3)
-                    {
-                        Console.WriteLine($"[Lv.{Program.Mlv3}][{Program.Mname3}] 가 공격을 시도 합니다!");
-                        player.Damage(Matk3); // 데미지 받는 것 불러오기 , 여기 수치도 리스트 값 처럼 불러와야함
-                        Thread.Sleep(700);
+                        if (m.IsAlive)
+                        {
+                            Console.WriteLine($"[Lv.{m.Level}][{m.Name}] (이)가 공격을 시도 합니다!");
+                            Console.WriteLine();
+                            player.Damage(m.Atk);
+                            Console.WriteLine();
+                            Thread.Sleep(300);
+                        }
                     }
 
                     Console.WriteLine();
@@ -144,13 +109,6 @@ namespace Sparta_Dungeon_TeamProject
 
                     Playerturn = true; // 턴 넘기기
                 }
-
-                if (KillMon >= 3) // 다 잡았을 때 성공으로 이동
-                {
-                    BattleSuccessUI();
-                    BettleLoop = false;
-                    break;
-                }
             }
         }
         static void PlayerAttack()
@@ -159,182 +117,55 @@ namespace Sparta_Dungeon_TeamProject
             Console.WriteLine();
             Console.WriteLine("**현재 일반 공격을 시도하는 중 입니다.**");
             Console.WriteLine();
-            Console.WriteLine($"1. [Lv.{Mlv1}][{Mname1}] {GetMonsterStatus(Mhp1, Mlife1)}");
-            Console.WriteLine($"2. [Lv.{Mlv2}][{Mname2}] {GetMonsterStatus(Mhp2, Mlife2)}");
-            Console.WriteLine($"3. [Lv.{Mlv3}][{Mname3}] {GetMonsterStatus(Mhp3, Mlife3)}");
+
+            PrintMonsters(); // 몬스터 출력
+
             Console.WriteLine();
             Console.WriteLine("0. 취소");
             Console.WriteLine();
             Console.WriteLine("대상을 선택해주세요.");
 
-            int result = CheckInput(0, 3); // CheckInput의 숫자를 0, [전투용 리스트에서 뽑힌 몬스터 마릿수] 로 설정해야 할 듯
-            switch (result)
+            int result = CheckInput(0, battleMonsters.Count);
+
+            if (result == 0)
             {
-                case 0:
-                    Program.Battle();
-                    break;
-
-                case 1:
-
-                    Program.Mhp1 -= Program.player.Atk; // 몬스터의 체력 값을 플레이어 공격력 값 만큼 뺀다.
-
-                    if (Program.Mhp1 <= 0) // 몬스터의 체력이 0보다 작거나 같다면
-                    {
-                        if (Mlife1) // 몬스터가 살아있다면
-                        {
-                            Console.WriteLine($"[Lv.{Program.Mlv1}][{Program.Mname1}] 에게 {Program.player.Atk} 만큼 피해를 입혔다!");
-                            Thread.Sleep(700);
-                            Console.WriteLine();
-                            Console.WriteLine($"[Lv.{Program.Mlv1}][{Program.Mname1}] 은(는) 일격을 맞고 사망했다!"); // 사망 메시지 출력
-                            KillMon += 1; // 몬스터 처치 값 +1 추가
-                            Mlife1 = false; // 몬스터가 사망했으니 생존 값을 false로 변경
-                            Monnum--; // 몬스터 마릿수 감소 // 추후에 배틀 몬스터 리스트에서 이 몬스터를 빼는 과정으로 수정
-                            Thread.Sleep(700);
-
-                            // 몬스터 보상을 출력한다고 하면 여기에 출력하면 될 것 같음
-
-                            if (Monnum > 0) // 필드에 존재하는 몬스터의 수가 0보다 크다면
-                            {
-                                Playerturn = false; // 플레이어의 공격이 끝났으니 플레이어 턴 값을 false 로 변경
-                                return;
-                            }
-                            else // 필드에 존재하는 몬스터의 수가 0 이라면
-                            {
-                                BattleSuccessUI(); // 배틀 성공 창을 띄움
-                                return;
-                            }
-                        }
-                        else // 몬스터가 죽어있다면
-                        {
-                            Console.WriteLine();
-                            Console.WriteLine("이미 사망한 몬스터는 공격할 수 없습니다.");
-                            Console.WriteLine();
-                            Thread.Sleep(500);
-                        }
-                    }
-                    else // 몬스터의 체력이 0보다 크다면
-                    {
-                        Console.WriteLine($"당신이 휘두른 검은 바람을 가르며 목표물을 베었다!");
-                        Console.WriteLine();
-                        Thread.Sleep(700);
-                        Console.WriteLine($"[Lv.{Program.Mlv1}][{Program.Mname1}] 에게 {Program.player.Atk} 만큼 피해를 입혔다!");
-                        Console.WriteLine();
-                        Thread.Sleep(700);
-                        Console.WriteLine($"[Lv.{Program.Mlv1}][{Program.Mname1}] 의 현재 HP [{Program.Mhp1}]");
-                        Thread.Sleep(700);
-                        Playerturn = false;
-                        Program.Battle();
-                    }
-                    break;
-
-                case 2:
-
-                    Program.Mhp2 -= Program.player.Atk; // 몬스터의 체력 값을 플레이어 공격력 값 만큼 뺀다.
-
-                    if (Program.Mhp2 <= 0) // 몬스터의 체력이 0보다 작거나 같다면
-                    {
-                        if (Mlife2) // 몬스터가 살아있다면
-                        {
-                            Console.WriteLine($"[Lv.{Program.Mlv2}][{Program.Mname2}] 에게 {Program.player.Atk} 만큼 피해를 입혔다!");
-                            Thread.Sleep(700);
-                            Console.WriteLine();
-                            Console.WriteLine($"[Lv.{Program.Mlv2}][{Program.Mname2}] 은(는) 일격을 맞고 사망했다!"); // 사망 메시지 출력
-                            KillMon += 1; // 몬스터 처치 값 +1 추가
-                            Mlife2 = false; // 몬스터가 사망했으니 생존 값을 false로 변경
-                            Monnum--; // 몬스터 마릿수 감소 // 추후에 배틀 몬스터 리스트에서 이 몬스터를 빼는 과정으로 수정
-                            Thread.Sleep(700);
-
-                            // 몬스터 보상을 출력한다고 하면 여기에 출력하면 될 것 같음
-
-                            if (Monnum > 0) // 필드에 존재하는 몬스터의 수가 0보다 크다면
-                            {
-                                Playerturn = false; // 플레이어의 공격이 끝났으니 플레이어 턴 값을 false 로 변경
-                                return;
-                            }
-                            else // 필드에 존재하는 몬스터의 수가 0 이라면
-                            {
-                                return;
-                            }
-                        }
-                        else // 몬스터가 죽어있다면
-                        {
-                            Console.WriteLine();
-                            Console.WriteLine("이미 사망한 몬스터는 공격할 수 없습니다.");
-                            Console.WriteLine();
-                            Thread.Sleep(500);
-                        }
-                    }
-                    else // 몬스터의 체력이 0보다 크다면
-                    {
-                        Console.WriteLine($"당신이 휘두른 검은 바람을 가르며 목표물을 베었다!");
-                        Console.WriteLine();
-                        Thread.Sleep(700);
-                        Console.WriteLine($"[Lv.{Program.Mlv2}][{Program.Mname2}] 에게 {Program.player.Atk} 만큼 피해를 입혔다!");
-                        Console.WriteLine();
-                        Thread.Sleep(700);
-                        Console.WriteLine($"[Lv.{Program.Mlv2}][{Program.Mname2}] 의 현재 HP [{Program.Mhp2}]");
-                        Thread.Sleep(700);
-                        Playerturn = false;
-                        Program.Battle();
-                    }
-                    break;
-
-                case 3:
-
-                    Program.Mhp3 -= Program.player.Atk; // 몬스터의 체력 값을 플레이어 공격력 값 만큼 뺀다.
-
-                    if (Program.Mhp3 <= 0) // 몬스터의 체력이 0보다 작거나 같다면
-                    {
-                        if (Mlife3) // 몬스터가 살아있다면
-                        {
-                            Console.WriteLine($"[Lv.{Program.Mlv3}][{Program.Mname3}] 에게 {Program.player.Atk} 만큼 피해를 입혔다!");
-                            Thread.Sleep(700);
-                            Console.WriteLine();
-                            Console.WriteLine($"[Lv.{Program.Mlv3}][{Program.Mname3}] 은(는) 일격을 맞고 사망했다!"); // 사망 메시지 출력
-                            KillMon += 1; // 몬스터 처치 값 +1 추가
-                            Mlife3 = false; // 몬스터가 사망했으니 생존 값을 false로 변경
-                            Monnum--; // 몬스터 마릿수 감소 // 추후에 배틀 몬스터 리스트에서 이 몬스터를 빼는 과정으로 수정
-                            Thread.Sleep(700);
-
-                            // 몬스터 보상을 출력한다고 하면 여기에 출력하면 될 것 같음
-
-                            if (Monnum > 0) // 필드에 존재하는 몬스터의 수가 0보다 크다면
-                            {
-                                Playerturn = false; // 플레이어의 공격이 끝났으니 플레이어 턴 값을 false 로 변경
-                                return;
-                            }
-                            else // 필드에 존재하는 몬스터의 수가 0 이라면
-                            {
-                                return;
-                            }
-                        }
-                        else // 몬스터가 죽어있다면
-                        {
-                            Console.WriteLine();
-                            Console.WriteLine("이미 사망한 몬스터는 공격할 수 없습니다.");
-                            Console.WriteLine();
-                            Thread.Sleep(500);
-                        }
-                    }
-                    else // 몬스터의 체력이 0보다 크다면
-                    {
-                        Console.WriteLine($"당신이 휘두른 검은 바람을 가르며 목표물을 베었다!");
-                        Console.WriteLine();
-                        Thread.Sleep(700);
-                        Console.WriteLine($"[Lv.{Program.Mlv3}][{Program.Mname3}] 에게 {Program.player.Atk} 만큼 피해를 입혔다!");
-                        Console.WriteLine();
-                        Thread.Sleep(700);
-                        Console.WriteLine($"[Lv.{Program.Mlv3}][{Program.Mname3}] 의 현재 HP [{Program.Mhp3}]");
-                        Thread.Sleep(700);
-                        Playerturn = false;
-                        Program.Battle();
-                    }
-                    break;
+                return;
             }
+
+            Monster target = battleMonsters[result - 1];
+
+            if (!target.IsAlive)
+            {
+                Console.WriteLine();
+                Console.WriteLine("이미 사망한 몬스터는 공격할 수 없습니다.");
+                Console.WriteLine();
+                Thread.Sleep(500);
+                Playerturn = true;
+                return;
+            }
+
+            target.Hp -= player.Atk; // 몬스터를 때리는 플레이어 데미지 계산식
+
+            Console.WriteLine();
+            Console.WriteLine($"[Lv.{target.Level}][{target.Name}] 에게 {player.Atk} 만큼 피해를 입혔다!");
+            Thread.Sleep(700);
+
+            if (target.Hp <= 0)
+            {
+                target.IsAlive = false;
+                Console.WriteLine();
+                Console.WriteLine($"[Lv.{target.Level}][{target.Name}] (은)는 일격을 맞고 사망했다!");
+                Thread.Sleep(700);
+                KillMon++;
+
+                if (battleMonsters.All(m => !m.IsAlive))
+                {
+                    BattleSuccessUI();
+                    return;
+                }
+            }
+            Playerturn = false;
         }
-
-
-
 
         public static void BattleSuccessUI()
         {
@@ -342,11 +173,11 @@ namespace Sparta_Dungeon_TeamProject
             Console.WriteLine();
             Console.WriteLine("전투 승리!");
             Console.WriteLine();
-            Console.WriteLine($"던전에서 몬스터를 {KillMon} 마리 만큼 처치 하셨습니다.");
+            Console.WriteLine($"현재 층에서 몬스터를 {KillMon} 마리 만큼 처치 하셨습니다.");
             Console.WriteLine();
             Console.WriteLine($"Lv.{player.Level} [{player.Name}]");
             Console.WriteLine();
-            Console.WriteLine("0. 다음");
+            Console.WriteLine("0. 다음 층으로 이동하기");
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
             KillMon = 0;
@@ -380,28 +211,53 @@ namespace Sparta_Dungeon_TeamProject
                     break;
             }
         }
+        static void PrintMonsters()
+        {
+            for (int i = 0; i < battleMonsters.Count; i++)
+            {
+                var m = battleMonsters[i];
+                Console.WriteLine($"[{i + 1}] [Lv.{m.Level}][{m.Name}] {GetMonsterStatus(m)}");
+            }
+        }
+    }
+    public static class MonsterSpawner
+    {
+        public static List<Monster> SpawnMonsters(string difficulty)
+        {
+            Random rand = new Random();
+            List<MonsterType> allowedTypes;
+            int monsterCount;
+
+            switch (difficulty.ToLower())
+            {
+                case "easy":
+                    allowedTypes = new List<MonsterType> { MonsterType.Goblin }; // 고블린만 소환
+                    monsterCount = rand.Next(1, 4); // 1~3마리 소환
+                    break;
+                case "normal":
+                    allowedTypes = new List<MonsterType> { MonsterType.Goblin, MonsterType.Orc }; // 고블린과 오크만 소환
+                    monsterCount = rand.Next(2, 4); // 2~3마리 소환
+                    break;
+                case "hard":
+                    allowedTypes = Enum.GetValues(typeof(MonsterType)).Cast<MonsterType>().ToList(); // 모든 몬스터 소환
+                    monsterCount = rand.Next(3, 5); // 3~4마리 소환
+                    break;
+
+                default:
+                    allowedTypes = new List<MonsterType> { MonsterType.Goblin };
+                    monsterCount = rand.Next(1, 3); // 기본 : 1~2마리 소환
+                    break;
+            }
+
+            List<Monster> monsters = new List<Monster>();
+            for (int i = 0; i < monsterCount; i++)
+            {
+                var type = allowedTypes[rand.Next(allowedTypes.Count)]; // 난이도에 맞는 몬스터 타입 선택
+                monsters.Add(MonsterFactory.CreateMonster(type)); // 해당 타입의 몬스터 생성
+            }
+
+            return monsters;
+        }
+
     }
 }
-//
-//public bool PlayerAlive = true;
-//
-//public void Damage(int amount)
-//{
-//    if (Hp > 0)
-//    {
-//        Hp -= amount;
-//        Console.WriteLine();
-//        Console.WriteLine($"[{Name} 은(는) {amount} 만큼 피해를 입었습니다!");
-//        Console.WriteLine();
-//        Console.WriteLine($"[현재 HP {Hp}]");
-//        Console.WriteLine();
-
-//        if (Hp <= 0)
-//        {
-//            PlayerAlive = false;
-//            Program.BattleFailureUI();
-//            return;
-//        }
-//    }
-
-// Player.cs 에서 데미지를 위와 같이 코드를 추가해주면 정상 작동
--- Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Monster.cs
+++ Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Monster.cs
@@ -8,27 +8,30 @@ using System.Threading.Tasks;
 namespace Sparta_Dungeon_TeamProject
 {
 
-  
+
     public class Monster
     {
+        // 몬스터의 이름
+        public string Name { get; protected set; }
+
         // 몬스터 기본 스탯
-        public int Level { get;}
-        public int Atk { get; }
-        public int Def { get; }
-        public int Hp { get; }
+        public int Level { get; protected set; }
+        public int Atk { get; protected set; }
+        public int Def { get; protected set; }
+        public int Hp { get; set; }
 
 
         // 레벨 상승에 따른 최종 스탯 계산을 위한 float값 추가
-        public float FinalAtk { get; }
-        public float FinalDef { get; }
-        public float FinalHp { get; }
+        public float FinalAtk { get; protected set; }
+        public float FinalDef { get; protected set; }
+        public float FinalHp { get; protected set; }
 
 
         // 몬스터 드랍 보상 (골드, 경험치)
-        public int Gold { get; }
-        public int Exp { get; }
-        public int DropGold { get; }
-        public int DropExp { get; }
+        public int Gold { get; protected set; }
+        public int Exp { get; protected set; }
+        public int DropGold { get; protected set; }
+        public int DropExp { get; protected set; }
 
 
         // 몬스터 생존 여부
@@ -38,13 +41,13 @@ namespace Sparta_Dungeon_TeamProject
         private static Random random = new Random();
 
         public Monster(
-            int minLevel, int maxLevel, 
-            int minAtk, int maxAtk, 
-            int minDef, int maxDef, 
-            int minHp, int maxHp, 
-            int minDropGold, int maxDropGold, 
-            int minDropExp, int maxDropExp, 
-            bool isAlive, 
+            int minLevel, int maxLevel,
+            int minAtk, int maxAtk,
+            int minDef, int maxDef,
+            int minHp, int maxHp,
+            int minDropGold, int maxDropGold,
+            int minDropExp, int maxDropExp,
+            bool isAlive,
             float orgin)
         {
             Level = random.Next(minLevel, maxLevel + 1);
@@ -66,44 +69,64 @@ namespace Sparta_Dungeon_TeamProject
             */
 
         }
-
-
-    }
-
-    class Goblin : Monster
-    {
-        public Goblin() : base(
-            minLevel: 1, maxLevel: 5,
-            minAtk: 3, maxAtk: 5,
-            minDef: 5, maxDef: 10,
-            minHp: 10, maxHp: 15,
-            minDropGold: 1000, maxDropGold: 3000,
-            minDropExp: 50, maxDropExp: 100, 
-            isAlive: true,
-            orgin: 0.1f)
+        public static class MonsterFactory
         {
-            
+            private static Random rand = new Random();
+
+            // 각 몬스터 타입을 생성하는 메서드
+            public static Monster CreateMonster(MonsterType type)
+            {
+                return type switch
+                {
+                    MonsterType.Goblin => new Goblin(),
+                    MonsterType.Orc => new Orc(),
+                    _ => throw new ArgumentException($"몬스터 타입 '{type}' 은(는) 존재하지 않습니다.")
+                };
+            }
+
+
+            public enum MonsterType
+            {
+                Goblin,
+                Orc
+            }
+
+            class Goblin : Monster
+            {
+                public Goblin() : base(
+                    minLevel: 1, maxLevel: 5,
+                    minAtk: 3, maxAtk: 5,
+                    minDef: 5, maxDef: 10,
+                    minHp: 10, maxHp: 15,
+                    minDropGold: 1000, maxDropGold: 3000,
+                    minDropExp: 50, maxDropExp: 100,
+                    isAlive: true,
+                    orgin: 0.1f)
+                {
+                    Name = "고블린";
+                }
+
+            }
+
+
+            class Orc : Monster
+            {
+                public Orc() : base(
+                    minLevel: 3, maxLevel: 5,
+                    minAtk: 5, maxAtk: 10,
+                    minDef: 10, maxDef: 20,
+                    minHp: 10, maxHp: 30,
+                    minDropGold: 2000, maxDropGold: 5000,
+                    minDropExp: 50, maxDropExp: 100,
+                    isAlive: true,
+                    orgin: 0.15f)
+                {
+                    Name = "오거";
+                }
+
+            }
+
         }
-
     }
-
-
-    class Orc : Monster
-    {
-        public Orc() : base(
-            minLevel: 3, maxLevel: 5,
-            minAtk: 5, maxAtk: 10,
-            minDef: 10, maxDef: 20,
-            minHp: 10, maxHp: 30,
-            minDropGold: 2000, maxDropGold: 5000,
-            minDropExp: 50, maxDropExp: 100,
-            isAlive: true,
-            orgin: 0.15f)
-        { 
-
-        }
-
-    }
-
- 
 }
+
