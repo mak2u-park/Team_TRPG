369ce03116120d4358aa7ce5d98c44331a746914
AgathaYi <tjsfid1030@naver.com>
2025-04-24 12:18:29 +0900

    chore : Job관련 클래스 파일 분리(스킬 제외)
    구조 문제 크게 없음.
    
    player.Job~ 은 원래 player없어도 됐음.

---
+++ Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Job.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_Dungeon_TeamProject
+{
+    // 직업 DB # SetData()
+    public enum JobType
+    {
+        전사 = 1,
+        마법사,
+        과학자,
+        대장장이,
+        영매사,
+    }
+
+    public class JobData
+    {
+        public int BaseAtk { get; }
+        public int BaseDef { get; }
+        public int BaseMaxHp { get; }
+        public int BaseMaxMp { get; }
+
+        public JobData(int atk, int def, int maxHp, int maxMp)
+        {
+            BaseAtk = atk;
+            BaseDef = def;
+            BaseMaxHp = maxHp;
+            BaseMaxMp = maxMp;
+        }
+    }
+
+    // 직업별 기본 능력치 DB # SetData()
+    public static class JobDB
+    {
+        public static Dictionary<JobType, JobData> Jobs = new Dictionary<JobType, JobData>
+            {   // 직업명 / 공격력 / 방어력 / 최대체력 / 최대마나
+                { JobType.전사, new JobData(5, 2, 70, 100 ) },
+                { JobType.마법사, new JobData(1, 1, 50, 0) },
+                { JobType.과학자, new JobData(8, 2, 50, 150) },
+                { JobType.대장장이, new JobData(1, 5, 70, 120) },
+                { JobType.영매사, new JobData(5, 4, 100, 75) }
+            };
+    }
+}
--- Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Player.cs
+++ Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/Player.cs
@@ -99,45 +99,6 @@ namespace Sparta_Dungeon_TeamProject
 
         }
 
-        // 직업 DB # SetData()
-        public enum JobType
-        {
-            침묵의영매사 = 1,
-            노쇄한대장장이,
-            뜨내기마법사,
-            불법과학자,
-            은퇴한전사,
-        }
-
-        public class JobData
-        {
-            public int BaseAtk { get; }
-            public int BaseDef { get; }
-            public int BaseMaxHp { get; }
-            public int BaseMaxMp { get; }
-
-            public JobData(int atk, int def, int maxHp, int maxMp)
-            {
-                BaseAtk = atk;
-                BaseDef = def;
-                BaseMaxHp = maxHp;
-                BaseMaxMp = maxMp;
-            }
-        }
-
-        // 직업별 기본 능력치 DB # SetData()
-        public static class JobDB
-        {
-            public static Dictionary<JobType, JobData> Jobs = new Dictionary<JobType, JobData>
-            {   // 직업명 / 공격력 / 방어력 / 최대체력 / 최대마나
-                { JobType.침묵의영매사, new JobData(5, 2, 70, 100 ) },
-                { JobType.노쇄한대장장이, new JobData(1, 1, 50, 0) },
-                { JobType.뜨내기마법사, new JobData(8, 2, 50, 150) },
-                { JobType.불법과학자, new JobData(1, 5, 70, 120) },
-                { JobType.은퇴한전사, new JobData(5, 4, 100, 75) }
-            };
-        }
-
         // 직업별 기본 스킬 지급 # SetData()
         public void GetExclusiveSkill()
         {
