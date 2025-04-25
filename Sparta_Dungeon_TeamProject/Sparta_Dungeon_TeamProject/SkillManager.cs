2ac8dd21d433e3398c768fea980286acaddc973c
백진환 <143722065+JH696@users.noreply.github.com>
2025-04-25 21:50:42 +0900

    스킬 텍스트 수정

--- Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/SkillManager.cs
+++ Sparta_Dungeon_TeamProject/Sparta_Dungeon_TeamProject/SkillManager.cs
@@ -29,16 +29,16 @@ namespace Sparta_Dungeon_TeamProject
             new SkillLibrary("능숙한 이간질", "시간이 지나도 그녀의 말솜씨는 여전하다.", 20, 4),
             new SkillLibrary("능숙한 이간질", "시간이 지나도 그녀의 말솜씨는 여전하다.", 20, 4),
             // 과학자 스킬 ( 8 ~ 11 )
-            new SkillLibrary("부식의 바늘", "치명적인 약품을 투약해, 적을 중독 시킵니다.", 20, 4),
-            new SkillLibrary("", ".", 20, 4),
-            new SkillLibrary("즉효성 유전자 교란", "적을 피실험체로 사용해 유전자를 재구성 시킵니다.", 20, 4),
-            new SkillLibrary("세포 분열 촉진제", "부작용을 감수하고, 병약한 세포를 촉진 시키는 약물을 복용합니다.", 20, 4),
+            new SkillLibrary("부식의 바늘", "치명적인 약품을 투약해, 적을 중독 시킵니다.", 40, 3),
+            new SkillLibrary("적응형 돌연변이", "현재 상황에 적합한 형태로 몸을 변화시킵니다.", 40, 5),
+            new SkillLibrary("즉효성 유전자 교란", "적을 피실험체로 사용해 유전자를 재구성 시킵니다.", 30, 4),
+            new SkillLibrary("세포 분열 촉진제", "부작용을 감수하고, 병약한 세포를 촉진 시키는 약물을 복용합니다.", 60, 5),
             // 대장장이 스킬 ( 12 )
             new SkillLibrary("잃어버린 불꽃", "자신에 체력이 낮을 때, 일정 시간동안 공격력을 크게 상승시킵니다.", 0, 0),
             // 영매사 스킬 ( 13 ~ 16 )
-            new SkillLibrary("", "이 땅에 쓰러진채 끝맺지 못했던 춤을 재현한다.", 20, 4),
-            new SkillLibrary("", ".", 20, 4),
-            new SkillLibrary("능숙한 이간질", "시간이 지나도 그녀의 말솜씨는 여전하다.", 20, 4),
+            new SkillLibrary("전하고 싶었던 말", "그녀가 쓰러진 이들의 목소리를 들려줍니다.", 30, 2),
+            new SkillLibrary("쇠와 꽃은 기억하리", "이 땅에 .", 20, 4),
+            new SkillLibrary("저편에 메아리", "시간이 지나도 그녀의 말솜씨는 여전하다.", 20, 4),
             new SkillLibrary("능숙한 이간질", "시간이 지나도 그녀의 말솜씨는 여전하다.", 20, 4),
         };
 
@@ -46,10 +46,10 @@ namespace Sparta_Dungeon_TeamProject
 
         public static Dictionary<JobType, List<SkillLibrary>> JobSkills = new()
         {
-            { JobType.전사, AllSkills.GetRange(1, 4) },
-            { JobType.마법사, AllSkills.GetRange(5, 8) },
-            { JobType.과학자, AllSkills.GetRange(11, 14) },
-            { JobType.대장장이, AllSkills.GetRange(1, 4) },
+            { JobType.전사, AllSkills.GetRange(0, 3) },
+            { JobType.마법사, AllSkills.GetRange(4, 7) },
+            { JobType.과학자, AllSkills.GetRange(8, 11) },
+            { JobType.대장장이, AllSkills.GetRange(12,12) },
             { JobType.영매사, AllSkills.GetRange(13, 16) }
         };
 
@@ -70,11 +70,11 @@ namespace Sparta_Dungeon_TeamProject
         }
         public static void FirstScientistSkill(Player player)
         {
-            player.Skills.Add(GetSkillByName("."));
+            player.Skills.Add(GetSkillByName("부식의 바늘."));
         }
         public static void FirstBlacksmithSkill(Player player)
         {
-            player.Skills.Add(GetSkillByName("."));
+            player.Skills.Add(GetSkillByName("잃어버린 불꽃."));
         }
         public static void FirstWhispererSkill(Player player)
         {
